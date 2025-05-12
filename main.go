5990ff22b006d7def19eb1a375f8957c491373c1
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-05-12 12:48:35 +0100

    feat: Add new bootstrap server with authentication

--- main.go
+++ main.go
@@ -8,36 +8,81 @@ import (
 )
 
 func main() {
-	cloudInitYaml, err := os.ReadFile("cloud-init.yaml")
+	devTestCloudInitYaml, err := os.ReadFile("dev-test/cloud-init.yaml")
+	if err != nil {
+		log.Fatalf("failed to load cloud-init.yaml: %s", err)
+	}
+
+	devTestAuthCloudInitYaml, err := os.ReadFile("dev-test-auth/cloud-init.yaml")
 	if err != nil {
 		log.Fatalf("failed to load cloud-init.yaml: %s", err)
 	}
 
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		getSshKeysResult, err := digitalocean.GetSshKeys(ctx, &digitalocean.GetSshKeysArgs{}, nil)
-		if err != nil {
+		if err := configureDevTestBootstrapSrv(ctx, string(devTestCloudInitYaml)); err != nil {
 			return err
 		}
 
-		var sshFingerprints []string
-		for _, key := range getSshKeysResult.SshKeys {
-			sshFingerprints = append(sshFingerprints, key.Fingerprint)
-		}
-
-		_, err = digitalocean.NewDroplet(ctx, "kitsune2-bootstrap-srv", &digitalocean.DropletArgs{
-			Image:    pulumi.String("ubuntu-24-04-x64"),
-			Name:     pulumi.String("kitsune2-bootstrap-srv"),
-			Region:   pulumi.String(digitalocean.RegionFRA1),
-			Size:     pulumi.String(digitalocean.DropletSlugDropletS2VCPU2GB),
-			Ipv6:     pulumi.Bool(true),
-			Tags:     pulumi.StringArray{pulumi.String("network-services")},
-			SshKeys:  pulumi.ToStringArray(sshFingerprints),
-			UserData: pulumi.String(cloudInitYaml),
-		})
-		if err != nil {
+		if err := configureDevTestAuthBootstrapSrv(ctx, string(devTestAuthCloudInitYaml)); err != nil {
 			return err
 		}
 
 		return nil
 	})
+}
+
+func configureDevTestBootstrapSrv(ctx *pulumi.Context, devTestCloudInitYaml string) error {
+	getSshKeysResult, err := digitalocean.GetSshKeys(ctx, &digitalocean.GetSshKeysArgs{}, nil)
+	if err != nil {
+		return err
+	}
+
+	var sshFingerprints []string
+	for _, key := range getSshKeysResult.SshKeys {
+		sshFingerprints = append(sshFingerprints, key.Fingerprint)
+	}
+
+	_, err = digitalocean.NewDroplet(ctx, "kitsune2-bootstrap-srv", &digitalocean.DropletArgs{
+		Image:    pulumi.String("ubuntu-24-04-x64"),
+		Name:     pulumi.String("kitsune2-bootstrap-srv"),
+		Region:   pulumi.String(digitalocean.RegionFRA1),
+		Size:     pulumi.String(digitalocean.DropletSlugDropletS2VCPU2GB),
+		Ipv6:     pulumi.Bool(true),
+		Tags:     pulumi.StringArray{pulumi.String("network-services")},
+		SshKeys:  pulumi.ToStringArray(sshFingerprints),
+		UserData: pulumi.String(devTestCloudInitYaml),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func configureDevTestAuthBootstrapSrv(ctx *pulumi.Context, devTestAuthCloudInitYaml string) error {
+	getSshKeysResult, err := digitalocean.GetSshKeys(ctx, &digitalocean.GetSshKeysArgs{}, nil)
+	if err != nil {
+		return err
+	}
+
+	var sshFingerprints []string
+	for _, key := range getSshKeysResult.SshKeys {
+		sshFingerprints = append(sshFingerprints, key.Fingerprint)
+	}
+
+	_, err = digitalocean.NewDroplet(ctx, "kitsune2-bootstrap-srv-auth", &digitalocean.DropletArgs{
+		Image:    pulumi.String("ubuntu-24-04-x64"),
+		Name:     pulumi.String("kitsune2-bootstrap-srv-auth"),
+		Region:   pulumi.String(digitalocean.RegionFRA1),
+		Size:     pulumi.String(digitalocean.DropletSlugDropletS2VCPU2GB),
+		Ipv6:     pulumi.Bool(true),
+		Tags:     pulumi.StringArray{pulumi.String("network-services")},
+		SshKeys:  pulumi.ToStringArray(sshFingerprints),
+		UserData: pulumi.String(devTestAuthCloudInitYaml),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
